462f710c25d02ddc1ad490b82fb8747b483bc1e9
NathanRCross <NRCross17@outlook.com>
2016-04-26 08:48:15 -0700

    Added files via upload

---
+++ 2DArrayDemo.java
@@ -0,0 +1,30 @@
+//name file: ArrayDemo.java
+class Array2DDemo {
+	public static void main (String args[]){
+		int i;
+		int[] eye;
+		eye = new int[16];
+			eye[0]= 80;
+			eye[1]= 39;
+			eye[2]= 78;
+			eye[3]= 21;
+			eye[4]= 58;
+			eye[5]= 69;
+			eye[6]= 33;
+			eye[7]= 90;
+			eye[8]= 49;
+			eye[9]= 6;
+			eye[10]= 7;
+			eye[11]= 10;
+			eye[12]= 50;
+			eye[13]= 65;
+			eye[14]= 12;
+			eye[15]= 25;
+			
+			for (i = 0; i<16; i++){
+				System.out.print(eye[i]);
+				System.out.print(" ");
+		}
+	}
+}		
+		
---
+++ Array2DDemo.java
@@ -0,0 +1,15 @@
+//name file: ArrayDemo.java
+class Array2DDemo {
+	public static void main (String args[]){
+		int r,c;
+		int[][] eye;
+		eye = new int[8][8];
+			eye[8][8] = {
+			
+			for (r = 0; r <8; r++){
+				System.out.print(eye[i]);
+				System.out.print(" ");
+		};
+	}
+}		
+		
